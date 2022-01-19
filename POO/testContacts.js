d660720c16373edbc0502bf719e71508586d88d6
PascualVera <pascualvera.994@gmail.com>
2022-01-19 11:45:06 +0100

    Correciones

--- POO/testContacts.js
+++ POO/testContacts.js
@@ -1,3 +1,6 @@
 let { Contacts: Contacts } = require("./contacts");
+let { Person: Person } = require("./person");
 let contact = new Contacts();
+contact.personas[0] = new Person();
+contact.personas[1] = new Person();
 contact.printPersons();
