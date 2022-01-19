8b6f5b68fbcf37d1cb534497d786dbec8013f3f2
PascualVera <pascualvera.994@gmail.com>
2022-01-19 14:45:49 +0100

    Valores añadidos

--- POO/testContacts.js
+++ POO/testContacts.js
@@ -1,6 +1,11 @@
 let { Contacts: Contacts } = require("./contacts");
 let { Person: Person } = require("./person");
 let contact = new Contacts();
-contact.personas[0] = new Person();
-contact.personas[1] = new Person();
+contact.personas[0] = new Person(70, 170);
+contact.personas[0].nombre = "Pascual";
+contact.personas[0].apellidos = "Vera Herrera";
+
+contact.personas[1] = new Person(120, 200);
+contact.personas[1].nombre = "The Rock";
+contact.personas[1].apellidos = "A secas";
 contact.printPersons();
