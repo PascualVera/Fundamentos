84d160ca9c4d4fe9482a2269ce92672ab61ca278
PascualVera <pascualvera.994@gmail.com>
2022-01-21 11:13:14 +0100

    Obligatorios typescript

--- TypeScript/contactsTest.ts
+++
@@ -1,6 +0,0 @@
-import { Person } from "./person";
-import { Contacts } from "./contacts";
-let contacts = new Contacts();
-contacts.people[0] = new Person("Jose", 2052, "Belén");
-contacts.people[1] = new Person("Maria", 2050, "Nazaret");
-contacts.printContacts();
--- TypeScript/personTest.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var person_1 = require("./person");
-var pascual = new person_1.Person("Pascual", 27, "1600 Pennsylvania Avenue NW, Washington, DC 20500, EE. UU.");
-pascual.printName();
-console.log(pascual.yearOfBirth(2021));
-pascual.setAddress("Calle Falsa 123");
-console.log(pascual.getAddress());
